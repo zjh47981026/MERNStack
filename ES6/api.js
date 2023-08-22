264595bbf58f4e5732e2ddfdce475b64fa1d57bf
zjh47981026 <zjh479810262@gmail.com>
2023-08-22 13:15:47 -0400

    update

---
+++ ES6/api.js
@@ -0,0 +1,20 @@
+//http https :=> hypertext type protocol
+//html -> hypertext markup language
+
+//http - stateless
+
+//we need to save information on server so that we can access it anywhere
+
+//user-
+//https://www.arit.userbase.com/api/user/searches - {search item} - post
+
+//https://www.arit.userbase.com/api/user/get/searches/userid  -get
+
+//saving information on ui/screen - representation
+
+//REST - Representational State Transfer protocol
+
+//Rest ful API's have below specifications -
+//it uses http
+//definitive verbs or http method - get/put/post/patch/delete
+//stateless 
