b877568348ebb41865055768d3956512ca8558cd
zjh47981026 <zjh479810262@gmail.com>
2023-08-28 13:19:40 -0400

    update

--- node-server/server.js
+++ node-server/server.js
@@ -8,7 +8,22 @@ app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
+//query params
+app.get('/queryparam', function(req, res) {
+    let query = req.query['name'];
+    res.send(`This is the name sent in query ${query}`);
+})
+
+//route params:/name - of the param
+app.get('/routeprm/:name/info', function(req, res) {
+    let routeParam = req.params["name"];
+    res.send(`This is the name sent to Route ${routeParam}`);
+})
+
 app.get('/helloapi', function (req, res) {
+   // console.log(req);
+    console.log(req.rawHeaders[11]);
+    let deviceType = req.rawHeaders[11]
     res.json({
         "Name" : "Jason",
         "Status" : 2000,
