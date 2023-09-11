14806f4e197ff9992fcf815c0860908e1e6729a9
zjh47981026 <zjh479810262@gmail.com>
2023-09-10 22:47:44 -0400

    update

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -5,15 +5,30 @@ import Header from "./CommonComponent/HeaderComponent"
 export default class Application extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            headerValue: "My First React App Page"
+        }
+        //dynamic object
+    }
+
+    changeText = () => {
+        this.setState({
+            headerValue: "New Title MERNStack"
+        })
     }
 
     render() {
         let name = "Van Duc Phan"
+        console.log("Creating virtual dom");
         //JSX - code, html + JS creating ta virtual dom
         return (
+            //when data is changed, a new Virtual DOM is created
+            //React will compare the old Virtual DOM and new Vitual DOM
+            //to rerender the page
             <>
                 <Header/>
-                <h1>My First React Application Page</h1>
+                <h1>{this.state.headerValue}</h1>
+                <button onClick={this.changeText}>Change Text</button>
                 <h2>Hello React {name}</h2>
             </>
         )
